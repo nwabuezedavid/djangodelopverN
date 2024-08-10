25631c9803e14c13026a124717e41c0588b388e2
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-08-10 10:58:07 +0100

    Rerge confls

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (  
     <html lang="en">
+      <script src="https://cdn.tailwindcss.com" defer></script>
+
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
       <body className="">
       <Contextnav >
